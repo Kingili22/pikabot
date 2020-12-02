91d8826eda41311da3b3484098f5a31c898559f0
Kingili22 <75387946+Kingili22@users.noreply.github.com>
2020-12-02 14:51:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('Ready!');
 });
-
 client.on('message', message => {
-	if (message.content === 'p!pikapic') {
-		message.channel.send('https://imgur.com/6cnl3Xa');
-	}
+    if (message.content === 'p!pikapic') {
+    pictures = ["https://imgur.com/25MqwKH", "https://imgur.com/6cnl3Xa", "https://imgur.com/kQeH1Bb", "https://imgur.com/l1L0yEf"];
+    const random = Math.floor(Math.random() * pictures.length);
+    message.channel.send(pictures[random]);
+    }
 });
 client.on('message', message => {
         if (message.content === 'p!ping') {
@@ -18,7 +19,12 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === 'p!version') {
-        message.channel.send('`The version is 1.1`');
+        message.channel.send('`The bots version is 1.3`');
     }
 });
-client.login('TOKEN');
+client.on('message', message => {
+    if (message.content === 'p!help') {
+        message.channel.send('We have 4 commands, p!help - tells you all the commands, p!version - send the current version, p!ping - Responds with Pong!, p!pikapic - Sends a random pikachu picture');
+    }
+});
+client.login('TOKEN')
