a275a9d3b34d5b4a8900b954a4740ba8a192c5f8
Kingili22 <75387946+Kingili22@users.noreply.github.com>
2020-12-03 10:51:49 -0600

    Update index.js
    Added a bunch of new commands

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ client.once('ready', () => {
 });
 client.on('message', message => {
     if (message.content === 'p!pikapic') {
-    pictures = ["https://imgur.com/25MqwKH", "https://imgur.com/6cnl3Xa", "https://imgur.com/kQeH1Bb", "https://imgur.com/l1L0yEf"];
+    pictures = ["https://imgur.com/25MqwKH", "https://imgur.com/6cnl3Xa", "https://imgur.com/kQeH1Bb", "https://imgur.com/l1L0yEf", "https://imgur.com/gQiA1nQ","https://imgur.com/qDqKSho","https://tenor.com/view/pikachu-pokemon-cali7-thunder-lightning-gif-15699421","https://tenor.com/view/detective-pikachu-investigation-pokemon-gif-13601363", "https://imgur.com/ZhYgje8","https://tenor.com/view/detective-pikachu-investigation-pokemon-gif-13601363", "https://imgur.com/88XHMq0", "https://tenor.com/view/pikachu-yeah-cheer-pokemon-pikachus-gif-17585470"];
     const random = Math.floor(Math.random() * pictures.length);
     message.channel.send(pictures[random]);
+    }
+});
+client.on('message', message => {
+    if (message.content === 'p!pokegif') {
+    pokemon = ["https://tenor.com/view/squirtle-pikachu-pokemon-dancing-happy-gif-15646320", "https://tenor.com/view/pika-pokemon-happy-high-five-gif-15448595", "https://tenor.com/view/deal-with-it-squirtle-pokemon-gif-6166819", "https://tenor.com/view/charmander-gif-5827908", "https://tenor.com/view/bulbasaur-pokemon-content-happy-nodding-gif-5483121", "https://tenor.com/view/pokemon-dragon-fire-chardizard-gif-7887070", "https://tenor.com/view/laugh-mew-pokemon-funny-hilarious-gif-15197015", "https://tenor.com/view/stretching-snorlax-anime-pokemon-cute-gif-17653080"];
+    const random = Math.floor(Math.random() * pokemon.length);
+    message.channel.send(pokemon[random]);
+    }
+});
+client.on('message', message => {
+    if (message.content === 'p!coinflip') {
+    coinflip = ["Heads!","Tails!"];
+    const random = Math.floor(Math.random() * coinflip.length);
+    message.channel.send(coinflip[random]);
     }
 });
 client.on('message', message => {
@@ -26,10 +40,26 @@ client.on('message', message => {
     }
 });
 client.on('message', message => {
+    if (message.content === 'p!patchnotes') {
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "Patchnotes starting from 2.0.0"
+          }});
+    }
+});
+client.on('message', message => {
+    if (message.content === 'p!developers') {
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "<@535235896530960395> is the main developer, people who contribute a lot get special mention here!"
+          }});
+    }
+});
+client.on('message', message => {
     if (message.content === 'p!version') {
         message.channel.send({embed: {
             color: 3447003,
-            description: "The bots current version is 1.4"
+            description: "The bots current version is 1.5.4"
           }});
     }
 });
@@ -37,7 +67,23 @@ client.on('message', message => {
     if (message.content === 'p!help') {
         message.channel.send({embed: {
             color: 3447003,
-            description: "We have 5 commands, p!help - tells you all the commands, p!version - send the current version, p!ping - Responds with Pong!, p!pikapic - Sends a random pikachu picture and p!github send the github repository link"
+            description: "MAKE SURE TO ONLY USE THIS COMMAND IN DESIGNATED BOT COMMANDS CHANNEL, RUN p!confirm TO RUN IT"
+          }});
+    }
+});
+client.on('message', message => {
+    if (message.content === 'p!confirm') {
+        ("p!help - tells you all the commands");
+        ("p!version - send the current version");
+        ("p!ping - Responds with Pong!");
+        ("p!pikapic - Sends a random pikachu picture");
+        ("p!github - sends the github repository link");
+        ("p!coinflip - flips a coin");
+        ("p!developers - mentions the main developers");
+        ("p!patchnotes - displays the patch notes")
+        message.channel.send({embed: {
+            color: 3447003,
+            description: "p!help - tells you all the commands, p!version - send the current version, p!ping - Responds with Pong!, p!pikapic - Sends a random pikachu picture, p!pokegif - sends a random pokemon gif, p!github - sends the github repository link, p!coinflip - flips a coin, p!developers - mentions the main developers, p!patchnotes - displays the patch notes"
           }});
     }
 });
